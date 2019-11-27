82ff4b86c49d92f170cc1fc7fe7ca4b8a7c1a9b0
HDElectronics <khadraouiibrahim@gmail.com>
2019-11-27 13:58:12 +0100

    now I set up the html heritage

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z=r%uu*+nei2u*1@7nqw-%z82n0&_o$yv!wuprt*)1*iok_mt!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
