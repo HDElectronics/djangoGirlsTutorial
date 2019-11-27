bbf6b2e0ed21c3c9971246e11fc30a586512c099
HDElectronics <khadraouiibrahim@gmail.com>
2019-11-27 21:13:17 +0100

    modification to load the css from pythonanywhere

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,4 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = "/home/btkhd/btkhd.pythonanywhere.com/blog/static"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
